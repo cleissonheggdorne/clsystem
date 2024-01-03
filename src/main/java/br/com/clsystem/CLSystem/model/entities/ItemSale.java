80c625c43d287b79abd98dc555d2855e996bb1fa
cleissonheggdorne <cleissonrosa@hotmail.com>
2024-01-02 22:56:32 -0300

    fix: corrigir nomenclatura campo tabela

--- src/main/java/br/com/clsystem/CLSystem/model/entities/ItemSale.java
+++ src/main/java/br/com/clsystem/CLSystem/model/entities/ItemSale.java
@@ -2,14 +2,20 @@ package br.com.clsystem.CLSystem.model.entities;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
@@ -57,7 +63,7 @@ public class ItemSale implements Serializable {
 	private Integer quantity;
 	
 	@NotNull
-	@Column(name="amounth")
+	@Column(name="amount")
 	private BigDecimal amount;
-	
+
 }
