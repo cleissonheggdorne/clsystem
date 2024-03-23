d40a94ae25a875cc9ca1670c2f3cefdb70bdc44f
cleissonheggdorne <cleissonrosa@hotmail.com>
2024-03-23 14:34:20 -0300

    Alt: Centralizada  Adição de preenchimento de informações do usuário e caixa direto na classe da navbar custom.

--- frontend/src/assets/js/commonComponents/navbarCustom.js
+++ frontend/src/assets/js/commonComponents/navbarCustom.js
@@ -1,3 +1,4 @@
+import UtilsStorage from '../Utils/UtilsStorage.js';
 class HeaderCustom extends HTMLElement{
     connectedCallback(){
         this.innerHTML = `<div class="row">
@@ -9,6 +10,9 @@ class HeaderCustom extends HTMLElement{
                                     </div>
                             </nav>
                         </div>`;
+        (UtilsStorage.userLogged)?NavbarUtils.fillInformationLoggin(UtilsStorage.getUser()):"";
+        (UtilsStorage.openedCashier)?NavbarUtils.fillInformationCashier(UtilsStorage.getCashier()):"";
+
     }
 }
 class NavbarCustom extends HTMLElement{
@@ -20,9 +24,43 @@ class NavbarCustom extends HTMLElement{
                                 <li class="collection-item"><a href="http://127.0.0.1:5500/frontend/src/view/pages/employee.html">Cadastro de Funcionários</a></li>
                             </ul>
                         </div>`;
+        //const navBarUtils = new NavbarUtils();
+        NavbarUtils.fillButtonOpenCloseCashier(UtilsStorage.openedCashier?"close":"open");
+    }
+    getSessionStorage(){
+        return sessionStorage;
     }
 }
-console.log("teste")
+class NavbarUtils{
+    constructor(){ }
+    
+    static fillButtonOpenCloseCashier(openClose){
+        const btnSpanOpenCloseCashier = document.getElementById("open-close-cashier");
+        btnSpanOpenCloseCashier.classList.add("white-text");
+        if(openClose === "open"){
+        btnSpanOpenCloseCashier.textContent = "Abrir Caixa";
+        btnSpanOpenCloseCashier.classList.add("light-green");
+        }else{
+        btnSpanOpenCloseCashier.textContent = "Fechar Caixa";
+        btnSpanOpenCloseCashier.classList.add("light-gray");
+        } 
+    }
+    static fillInformationLoggin(user){
+        const navInformation = document.getElementById("nav");
+        const liUser = document.createElement("li");
+        liUser.textContent = "Usuário: " + user.idEmployee+" "+user.nameEmployee+"ㅤ";
+        navInformation.appendChild(liUser); 
+    }
+    static fillInformationCashier(cashier){
+        const navInformation = document.getElementById("nav");
+        const liCashier = document.createElement("li");
+        liCashier.innerHTML = "";
+        liCashier.textContent = (cashier != null)? " Caixa: " + cashier.status + " Hora Abertura: "+cashier.dateHourOpenFormatted: "Não Há Registro de Caixa em Aberto";
+        navInformation.appendChild(liCashier); 
+    }
+}
+
 customElements.define("header-custom", HeaderCustom);
 customElements.define("navbar-custom", NavbarCustom);
+
 
