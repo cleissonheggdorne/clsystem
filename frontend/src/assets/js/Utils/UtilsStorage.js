fe188216a22a15519c2db6129e7786ddce13ad3e
cleissonheggdorne <cleissonrosa@hotmail.com>
2024-04-02 23:22:42 -0300

    Adição de métodos para verificaão de usuário, caixa e se determinada propriedade está presente no Storage

--- frontend/src/assets/js/Utils/UtilsStorage.js
+++ frontend/src/assets/js/Utils/UtilsStorage.js
@@ -15,14 +15,18 @@ export default class StorageUtils {
         return JSON.parse(sessionStorage.getItem("cashier"));
     }
 
-    static userLogged(sessionStorage){
-      const user = JSON.parse(sessionStorage.getItem("user"));
-      console.log(sessionStorage);
-      return (user != null);
+    static userLogged(){
+      const user = StorageUtils.existsPropertyStorage("user")? 
+                  JSON.parse(sessionStorage.getItem("user")) : "";
+      return user != "";
     }
-    static openedCashier(sessionStorage){
-      const cashier = JSON.parse(sessionStorage.getItem("cashier"));
-      console.log(sessionStorage);
-      return (cashier != null);
-  }
+    static openedCashier(){
+      const cashier = StorageUtils.existsPropertyStorage() ? 
+                  JSON.parse(sessionStorage.getItem("cashier"))
+                    : "";
+      return (cashier != "");
+    }
+    static existsPropertyStorage(property){
+      return sessionStorage.getItem(property) != undefined;
+    }
 }
