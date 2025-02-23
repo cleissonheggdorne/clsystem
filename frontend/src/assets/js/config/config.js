80ff4fbb7baea3d753a6565ed82989efc3ccbdbb
cleissonheggdorne <cleissonrosa@hotmail.com>
2025-02-23 18:19:13 -0300

    urls reconfiguradas

--- frontend/src/assets/js/config/config.js
+++ frontend/src/assets/js/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     // URL base do backend
-    backendBaseUrl: 'http://localhost:8081',
-    frontendUrl: 'http://127.0.0.1:5501/frontend/' 
+    backendBaseUrl: 'http://144.217.81.7:8081',
+    frontendUrl: 'http://127.0.0.1:5500/frontend' 
 };
 
 // Exporta o objeto de configuração
