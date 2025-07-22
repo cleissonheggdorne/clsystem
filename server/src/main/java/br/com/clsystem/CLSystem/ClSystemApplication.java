b8312c380c4cac0bbb65e5e231aa018638896f45
cleissonheggdorne <cleissonrosa@hotmail.com>
2025-07-22 20:57:08 -0300

    Correção timezone e nome do arquivo compilado

--- server/src/main/java/br/com/clsystem/CLSystem/ClSystemApplication.java
+++ server/src/main/java/br/com/clsystem/CLSystem/ClSystemApplication.java
@@ -1,5 +1,7 @@
 package br.com.clsystem.CLSystem;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClSystemApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 		SpringApplication.run(ClSystemApplication.class, args);
-		 
 	}
 	
 
