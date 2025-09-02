c2c55392676a764afbda738e157224345303503c
Ubuntu <ubuntu@vps-41b3e76a.vps.ovh.ca>
2025-09-02 00:25:38 +0000

    Alteração rota para uso de https

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8081', // URL da API para produção
+    apiUrl: 'http://localhost:443', // URL da API para produção
   };
