120201f58847828dc3a880904daeffb1e0c79fd9
cleissonheggdorne <cleissonrosa@hotmail.com>
2025-09-04 18:52:33 -0300

    Correção base url backend em produção

--- client/src/app/environments/environment.prod.ts
+++ client/src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://digihous.com.br:8081', // URL da API para produção
+    apiUrl: 'http://digihous.com.br:8081', // URL da API para produção
   };
